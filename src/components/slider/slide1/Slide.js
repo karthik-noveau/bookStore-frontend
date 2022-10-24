ae36819a3cde2f9a5a34e500c44df7daab97a6b4
Karthik <karthik5112001@gmail.com>
2022-10-24 12:30:00 +0530

    commit

--- src/components/slider/slide1/Slide.js
+++ src/components/slider/slide1/Slide.js
@@ -1,49 +1,95 @@
 
 import Slider from "react-slick";
-import {BooksData} from './SlideData';
+import { BooksData } from './SlideData';
 import './Slider.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 
 function Slide() {
+
   var settings = {
     dots: true,
     infinite: false,
     speed: 500,
-    slidesToShow: 7,
+    slidesToShow: 10, 
     slidesToScroll: 2,
     initialSlide: 0,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint:850,
         settings: {
-          slidesToShow: 5,
-          slidesToScroll: 2,
+          slidesToShow:6,
+          slidesToScroll: 5,
           infinite: true,
-          dots: true
+          dots: false
         }
       },
+     
+      {
+        breakpoint:480,
+        settings: {
+          slidesToShow:3,
+          slidesToScroll: 5,
+          infinite: true,
+          dots: false
+        }
+      },
+     
       {
         breakpoint: 600,
         settings: {
           slidesToShow:4,
-          slidesToScroll: 2,
-          initialSlide: 2
+          slidesToScroll: 5,
+          infinite: true,
+          dots: false
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 900,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 2
+          slidesToShow:6,
+          slidesToScroll: 12,
+          infinite: true,
+          dots: false
         }
-      }
+      },
+      {
+        breakpoint: 1650,
+        settings: {
+          slidesToShow:6,
+          slidesToScroll:9,
+          infinite: true,
+          dots: false
+        }
+      },
+    
+
+  
+     
+   
     ]
   };
   return (
-    <div className="slide_cont">
-     <h1>hai</h1>
+    <div className="slide_main_cont">
+ 
+      <div className="slide_cont">
+
+        <Slider {...settings}>
+          {
+            BooksData.map((item, index) => {
+              return (
+                <div>
+                  <img src={item.image} />
+                </div>
+              )
+            })
+          }
+
+        </Slider>
+      </div>
+
+
     </div>
   );
 }
--- src/components/slider/slide1/SlideData.js
+++ src/components/slider/slide1/SlideData.js
@@ -30,5 +30,37 @@ export const BooksData=[
     {
         text:"three",
         image:' https://firebasestorage.googleapis.com/v0/b/book-store-2701c.appspot.com/o/slider-1%2F9780689818769.jpg?alt=media&token=9e93839d-a240-40b8-a227-063747fa497c'
+    },
+    {
+        text:"one",
+        image:'https://firebasestorage.googleapis.com/v0/b/book-store-2701c.appspot.com/o/slider-1%2F9780062691194.jpg?alt=media&token=20b269d4-2345-4cf3-b759-1de3a589cc80'
+    },
+    {
+        text:"two",
+        image:' https://firebasestorage.googleapis.com/v0/b/book-store-2701c.appspot.com/o/slider-1%2F9780441569595.jpg?alt=media&token=03fc6995-8925-476e-8105-8a317ddde07d'
+    },
+    {
+        text:"three",
+        image:' https://firebasestorage.googleapis.com/v0/b/book-store-2701c.appspot.com/o/slider-1%2F9780689818769.jpg?alt=media&token=9e93839d-a240-40b8-a227-063747fa497c'
+    },
+    {
+        text:"one",
+        image:'https://firebasestorage.googleapis.com/v0/b/book-store-2701c.appspot.com/o/slider-1%2F9781250217318.jpg?alt=media&token=be670b54-ba4c-48b2-8b55-2ffa414d0c75'
+    },
+    {
+        text:"two",
+        image:'https://firebasestorage.googleapis.com/v0/b/book-store-2701c.appspot.com/o/slider-1%2F9781338157826.jpg?alt=media&token=2ac81856-f2bc-4bd7-a312-9661fb1a4791'
+    },
+    {
+        text:"one",
+        image:'https://firebasestorage.googleapis.com/v0/b/book-store-2701c.appspot.com/o/slider-1%2F9780062691194.jpg?alt=media&token=20b269d4-2345-4cf3-b759-1de3a589cc80'
+    },
+    {
+        text:"two",
+        image:' https://firebasestorage.googleapis.com/v0/b/book-store-2701c.appspot.com/o/slider-1%2F9780441569595.jpg?alt=media&token=03fc6995-8925-476e-8105-8a317ddde07d'
+    },
+    {
+        text:"three",
+        image:' https://firebasestorage.googleapis.com/v0/b/book-store-2701c.appspot.com/o/slider-1%2F9780689818769.jpg?alt=media&token=9e93839d-a240-40b8-a227-063747fa497c'
     }
 ];
